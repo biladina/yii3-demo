8194e06b3db883566ff6e89425bef63203e461b8
Wilmer Arambula <terabytesoftw@gmail.com>
2023-03-18 10:34:15 -0300

    More update docs.

--- resources/snippet/menu-flowbite.php
+++ resources/snippet/menu-flowbite.php
@@ -2,8 +2,6 @@
 
 declare(strict_types=1);
 
-use Yii\Component\Tag\Img;
-
 return [
     'activeClass()' => [
         'block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white',
